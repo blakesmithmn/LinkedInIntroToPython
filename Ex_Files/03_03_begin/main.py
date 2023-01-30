a6b781ad45c5a2cae69af8526ecf98d844ddc3e4
Blake Smith <blake.smith.mn@gmail.com>
2023-01-30 18:24:04 +0000

    learned how to turn CSV into JSON and back again!

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -1,7 +1,10 @@
-import csv
+# TURNING CSV INTO JSON
+
+import csv # read the CSV data
 import json # JSON module utility!
 from pprint import pprint
 
+# REMEMBER THIS IS A DICTIONARY
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
@@ -11,15 +14,15 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-einstein_json = json.dumps(EINSTEIN)
-back_to_dict = json.loads(einstein_json)
+einstein_json = json.dumps(EINSTEIN) #creating a variable that is the json of the einstein dictionary
+back_to_dict = json.loads(einstein_json) # creating a variable that convers the JSON back to dictionary
 print(einstein_json)
 pprint(back_to_dict)
 
-with open("laureates.csv", "r") as f:
-    reader = csv.DictReader(f)
-    laureates = list(reader)
+with open("laureates.csv", "r") as f: #bringing in CSV data in 'r' read mode
+    reader = csv.DictReader(f) # creating a dictionary using the data
+    laureates = list(reader) # creating an array of the dictionaries 
 
 
-with open("laureates.json", "w") as f:
-    json.dump(laureates, f, indent=2)
+with open("laureates.json", "w") as f: # opening the existing JSON file in 'w' write mode
+    json.dump(laureates, f, indent=2) # dump all of our information into the file ... from list laureates into file 'f' and level of indentation which is 2
