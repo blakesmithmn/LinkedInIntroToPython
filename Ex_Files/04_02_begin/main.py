28443dee2cf327fa18de73689614938819dca65c
Blake Smith <blake.smith.mn@gmail.com>
2023-01-30 18:56:04 +0000

    quickly turned the CSV data into a single page web application using python - with individual detail pages

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask # flask lets you quickly get things on the web
 
-app = Flask(__name__)
+app = Flask(__name__) # boilerplate - thankfully flask doesn't have a ton ... 
+
+@app.route("/") # this is how we would set up a route!
+def hello():
+  return "Hello, World" #what happens when we hit the route!
+
+
+app.run(debug=True)
