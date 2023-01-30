4d6a2f6e7b2918f0e4f2e2bdfcaa2f64f2349380
Blake Smith <blake.smith.mn@gmail.com>
2023-01-30 19:01:32 +0000

    used a couple lines of python to add a search functionality to search in the data

--- Ex_Files/04_03_begin/app.py
+++ Ex_Files/04_03_begin/app.py
@@ -18,12 +18,16 @@ def index():
 @app.route("/laureates/")
 def laureate_list():
     # template found in templates/laureate.html
-    results = []
-    if not request.args.get("surname"):
+    results = [] # empty list of laureates
+    if not request.args.get("surname"): # is there a param in the URL!?
         return jsonify(results)
 
     # Your code here!
-    return "your code here!"
+    search_string = request.args.get("surname").lower().strip()
 
+    for laureate in laureates:
+        if search_string in laureate["surname"].lower():
+            results.append(laureate)
+    return jsonify(results)
 
 app.run(debug=True)
